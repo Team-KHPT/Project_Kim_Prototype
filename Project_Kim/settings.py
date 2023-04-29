ae28ec515e87f4c64285dd3ec295962b61c4e701
siwonKH <kanghyunkim313@gmail.com>
2023-04-29 11:51:10 +0900

    Update settings.py

--- Project_Kim/settings.py
+++ Project_Kim/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
 
 INSTALLED_APPS = [
