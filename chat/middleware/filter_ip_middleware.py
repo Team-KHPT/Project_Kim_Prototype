80a37c9d67a1376c4e901ef590397016168d2766
siwonKH <kanghyunkim313@gmail.com>
2023-05-01 14:36:55 +0900

    UPDATE middleware

--- chat/middleware/filter_ip_middleware.py
+++ chat/middleware/filter_ip_middleware.py
@@ -1,19 +1,36 @@
 from django.core.exceptions import PermissionDenied
 
 
+def get_cf_connecting_ip(request):
+    ip = request.META.get('HTTP_CF_CONNECTING_IP')
+    if ip:
+        return ip
+    else:
+        return "no_cf_ip"
+
+
+def get_client_ip(request) -> str:
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        print("remote_addr")
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
 class FilterIPMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # 최초 설정 및 초기화
 
     def __call__(self, request):
         banned_ips = []
 
-        print("LOG:", request.META.get('HTTP_X_FORWARDED_FOR'), request.META.get('HTTP_CF_CONNECTING_IP'))
-        ip = request.META.get('HTTP_CF_CONNECTING_IP')
-        if ip in banned_ips:
+        ip1 = get_client_ip(request)
+        ip2 = get_cf_connecting_ip(request)
+        print("Client IP:", ip1, "CF_IP:", ip2)
+        if ip1 in banned_ips or ip2 in banned_ips:
             raise PermissionDenied
 
         response = self.get_response(request)
-
         return response
